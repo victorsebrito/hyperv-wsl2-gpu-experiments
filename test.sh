b3d277774d3bef52f3a99ea838a841027a2f17f7
Victor Brito <victorsebrito@gmail.com>
2025-01-19 23:56:31 -0300

    auto test and leave shell open if fails

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ffmpeg -y -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/card0 -i /assets/h264_sample.mp4 -c:v h264_vaapi /assets/output.mp4
+ffmpeg -y -hwaccel vaapi -hwaccel_output_format vaapi -hwaccel_device /dev/dri/card0 \
+    -i /assets/h264_sample.mp4 -c:v h264_vaapi /assets/output.mp4 \
+    || exec bash
